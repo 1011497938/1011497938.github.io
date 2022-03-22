6b1f55e20bb7812c3023cb88ebdec6bbbecddfe5
siwei <1011497938@qq.com>
2022-03-22 13:31:47 +0800

    before meeting

--- js/case 3.js
+++ js/case 3.js
@@ -1,24 +1,18 @@
-// 论文case 1
-// TODO: 优化下variable state点击的速度
+// QFT
 
-// QFT 的case
+qc.reset(7);
 
-qc.reset(9);
-
-var sender = qint.new(4, 'S')
-var receiver = qint.new(4, 'R')
+var sender = qint.new(3, 'S')
+var receiver = qint.new(3, 'R')
 var ancillary = qint.new(1, 'A')
 
 qc.write(0x0)
 
-let label = 'Generate data'
+let label = 'GenInfo'
 qc.startlabel(label)
-sender.ry(135, 0x4 | 0x8)
-// provider.ry(135, 0x4)
-
-// TODO: 反着了
-// let initial_state = tensorState(groundState(5), groundState(4, [7, 9]), )
-// qc.setState(initial_state)
+// sender.ry(135, 0x2 | 0x4)
+qc.ry(135, 0x2 | 0x4 | 0x8 | 0x16)
+qc.disableDisplay(label)
 qc.endlabel(label)
 
 
@@ -30,13 +24,13 @@ sender.invQFT()
 qc.endlabel(label)
 
 qc.nop()
-// label = 'send'
-// qc.startlabel(label)
+label = 'Send'
+qc.startlabel(label)
 sender.exchange(receiver)
-// qc.endlabel(label)
+qc.endlabel(label)
+qc.disableDisplay(label)
 qc.nop()
 
-// 加一个conditional ry
 
 label = 'QFT'
 qc.startlabel(label)
@@ -45,16 +39,23 @@ qc.endlabel(label)
 
 qc.nop()
 
-// label = 'entangle'
-// qc.startlabel(label)
-// qc.cnot(receiver.bits(0x4), ancillary.bits(0x1))
-// qc.endlabel(label)
-
-// qc.nop()
-// qc.nop()
-
-label = 'Increase high freq'
+label = 'A = R>=4'
 qc.startlabel(label)
-qc.cnot(receiver.bits(0x8), ancillary.bits(0x1))
+qc.cnot(receiver.bits(0x4), ancillary.bits(0x1))
+qc.endlabel(label)
+
+qc.nop()
+
+
+label = 'High freq++'
+qc.startlabel(label)
 receiver.add(1, ancillary.bits(0x1))
 qc.endlabel(label)
+
+
+let results = receiver.sample(4)
+results.forEach(result=>{
+    qc.print('QFT Frequency: ' + result)
+})
+
+results = receiver.read()
